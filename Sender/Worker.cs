708f3440120f44fdeae88a4d97a6c7627f9e583b
vvafel <vvafel@users.noreply.github.com>
2022-06-15 19:59:40 +0200

    Files

---
+++ Sender/Worker.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Sender.Interfaces;
+using Shared.Models;
+
+namespace Sender
+{
+    public class Worker : IConsumer<MessagesToSent>
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly ITelegramSenderService _telegramSenderService;
+
+        public Worker(ILogger<Worker> logger, ITelegramSenderService telegramSenderService)
+        {
+            _logger = logger;
+            _telegramSenderService = telegramSenderService;
+        }
+
+        public async Task Consume(ConsumeContext<MessagesToSent> context)
+        {
+            try
+            {
+                var messagesToSent = context.Message;
+
+                await _telegramSenderService.SendsAsync(messagesToSent.Users, messagesToSent.NewAdMessages);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+        }
+    }
+}
